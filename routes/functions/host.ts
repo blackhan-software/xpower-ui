8922c4326334fbab770c5a93bf5556aa20edb195
Kârūn The Rich <development@xpowermine.com>
2022-05-24 19:50:46 +0300

    XPower UI: use x-forwarded-proto[col]

--- routes/functions/host.ts
+++ routes/functions/host.ts
@@ -1,8 +1,10 @@
 import { Request } from 'express';
 
 export function host(req: Request): string {
-    const proxy = req.headers['x-forwarded-host'];
-    const authority = proxy ? proxy : req.headers.host;
-    return `${req.protocol}://${authority}`;
+    const xfp = req.headers['x-forwarded-proto'];
+    const protocol = xfp ? xfp : req.protocol;
+    const xfh = req.headers['x-forwarded-host'];
+    const authority = xfh ? xfh : req.headers.host;
+    return `${protocol}://${authority}`;
 }
 export default host;
