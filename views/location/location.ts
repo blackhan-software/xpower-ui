cb01b6405a233440293db9ff82d90ec366c6a0e2
Kârūn The Rich <development@xpowermine.com>
2022-07-07 22:16:37 +0300

    XPower UI: Location component

--- views/location/location.ts
+++ views/location/location.ts
@@ -1,5 +1,12 @@
 import { App } from '../../source/app';
+import { Blockchain } from '../../source/blockchain';
+import { delayed } from '../../source/functions';
 
+Blockchain.onceConnect(delayed(async function reloadLocation() {
+    const p = await Blockchain.provider;
+    p.on('chainChanged', () => location.reload());
+    p.on('accountsChanged', () => location.reload());
+}, 600));
 App.onTokenSwitch(function syncLocation(token) {
     const search = location.search
         .replace(/^$/, `?token=${token}`)
