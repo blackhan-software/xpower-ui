3dba835c501628063f20b76cae6740190c6606a7
Kârūn The Rich <development@xpowermine.com>
2022-09-04 18:11:16 +0300

    XPower UI: About functional

--- views/about/about.tsx
+++ views/about/about.tsx
@@ -9,7 +9,7 @@ import { Tokenizer } from '../../source/token';
 import { capitalize } from '../../routes/functions';
 import { link, script } from '../../source/functions';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 type markdownIt = (options: any) => {
     use: (plugin: any, options?: any) => void;
@@ -22,107 +22,107 @@ type Props = {
     token: Token;
     url?: URL;
 }
-type State = {
-    html: string;
-}
-export class UiAbout extends React.Component<
-    Props, State
-> {
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            html: ''
-        };
-        this.events(props);
-    }
-    async events(
-        { url, token }: Props
-    ) {
-        const md = await this.fetchMarkdown(url);
-        this.setState({
-            html: await this.renderMarkdown(md, token)
-        });
-    }
-    async fetchMarkdown(
-        url?: URL
-    ) {
-        if (!url) {
-            const $about_url = document.querySelector<HTMLElement>(
-                '#g-urls-about'
-            );
-            if (!$about_url) {
-                throw new Error('#g-urls-about missing');
-            }
-            const about_url = $about_url.dataset.value;
-            if (!about_url) {
-                throw new Error('#g-urls-about[data-value] missing');
-            }
-            url = new URL(about_url, location.origin);
-        }
-        const fetched = await fetch(url.toString());
-        return await fetched.text();
-    }
-    async renderMarkdown(
-        content: string, token: Token
-    ) {
-        await script(this.scripts.markdownIt);
-        const markdownit = global.markdownit as markdownIt;
-        const mdi = markdownit({
-            html: true, linkify: true, typographer: true
-        });
-        await script(this.scripts.markdownItAnchor);
-        const anchor = global.markdownItAnchor as markdownItAnchor;
-        mdi.use(anchor, {
-            permalink: anchor.permalink.linkInsideHeader({
-                symbol: '¶', ariaHidden: true
-            }),
-            level: 2
-        });
-        const html = mdi.render(
-            add_token(fix_katex(content))
+export function UiAbout(
+    { token, url }: Props
+) {
+    const [html, setHtml] = useState('');
+    const md_url = about_url(url);
+    const md = sessionStorage.getItem(md_url) ?? '';
+    if (md.length) {
+        renderMarkdown(md, token).then(
+            (html) => setHtml(html)
         );
-        function fix_katex(content: string) {
-            return content
-                .replace(/\\%/g, '\\\\%')
-                .replace(/\\#/g, '\\\\#');
-        }
-        function add_token(content: string) {
-            const token_lc = Tokenizer.lower(token);
-            return content
-                .replace(/{{TOKEN}}/g, token)
-                .replace(/{{token}}/g, token_lc)
-                .replace(/{{Token}}/g, capitalize(token_lc));
-        }
-        return html;
+    } else {
+        fetchMarkdown(md_url).then((md) => {
+            sessionStorage.setItem(md_url, md);
+            return renderMarkdown(md, token);
+        }).then(
+            (html) => setHtml(html)
+        );
     }
-    render() {
-        const { html } = this.state;
-        return <React.Fragment>
-            {Spinner({ show: !html })}
-            {this.$mdContent(html)}
-        </React.Fragment >;
-    }
-    $mdContent(
-        html: string
-    ) {
-        return <div id='md-content' dangerouslySetInnerHTML={{
-            __html: html
-        }} />;
-    }
-    componentDidUpdate() {
+    useEffect(() => {
         const $content = document.querySelector(
             '#md-content'
         );
-        this.renderKatex($content!);
+        renderKatex($content!);
         const anchor = location.hash;
-        this.scrollToAnchor(anchor);
+        scrollToAnchor(anchor);
+    }, [html]);
+    return <React.Fragment>
+        <div id='md-content' dangerouslySetInnerHTML={{
+            __html: html
+        }} />
+        {Spinner({ show: !html.length })}
+    </React.Fragment>;
+}
+function about_url(
+    url?: URL
+) {
+    if (!url) {
+        const $about_url = document.querySelector<HTMLElement>(
+            '#g-urls-about'
+        );
+        if (!$about_url) {
+            throw new Error('#g-urls-about missing');
+        }
+        const about_url = $about_url.dataset.value;
+        if (!about_url) {
+            throw new Error('#g-urls-about[data-value] missing');
+        }
+        url = new URL(about_url, location.origin);
     }
-    async renderKatex(
-        $content: Element
-    ) {
-        await link(this.styles.katex);
-        await script(this.scripts.katex);
-        await script(this.scripts.katexAutoRender);
+    return url.toString();
+}
+async function fetchMarkdown(
+    md_url: string
+) {
+    const fetched = await fetch(md_url);
+    return await fetched.text();
+}
+async function renderMarkdown(
+    content: string, token: Token
+) {
+    await Promise.all([
+        script(SCRIPTS.markdownIt),
+        script(SCRIPTS.markdownItAnchor)
+    ]);
+    const markdownit = global.markdownit as markdownIt;
+    console.assert(markdownit);
+    const mdi = markdownit({
+        html: true, linkify: true, typographer: true
+    });
+    const anchor = global.markdownItAnchor as markdownItAnchor;
+    console.assert(anchor);
+    mdi.use(anchor, {
+        permalink: anchor.permalink.linkInsideHeader({
+            symbol: '¶', ariaHidden: true
+        }),
+        level: 2
+    });
+    const html = mdi.render(
+        add_token(fix_katex(content))
+    );
+    function fix_katex(content: string) {
+        return content
+            .replace(/\\%/g, '\\\\%')
+            .replace(/\\#/g, '\\\\#');
+    }
+    function add_token(content: string) {
+        const token_lc = Tokenizer.lower(token);
+        return content
+            .replace(/{{TOKEN}}/g, token)
+            .replace(/{{token}}/g, token_lc)
+            .replace(/{{Token}}/g, capitalize(token_lc));
+    }
+    return html;
+}
+async function renderKatex(
+    $content: Element
+) {
+    await link(STYLES.katex);
+    await script(SCRIPTS.katex);
+    await script(SCRIPTS.katexAutoRender);
+    if (global.renderMathInElement) {
         global.renderMathInElement($content, {
             delimiters: [
                 { left: '$$', right: '$$', display: true },
@@ -133,50 +133,50 @@ export class UiAbout extends React.Component<
             throwOnError: false
         });
     }
-    scrollToAnchor(
-        anchor: string
-    ) {
-        if (anchor) {
-            location.hash = '';
-            location.hash = anchor;
-        }
+}
+function scrollToAnchor(
+    anchor: string
+) {
+    if (anchor) {
+        location.hash = '';
+        location.hash = anchor;
     }
-    styles = {
-        katex: {
-            href: 'https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css',
-            integrity: 'sha256-uik/hNqHWZldXh/0K35nqOSCff9F61/ZOFReqNOBgB0=',
-            crossOrigin: 'anonymous', rel: 'stylesheet'
-        }
+}
+const STYLES = {
+    katex: {
+        href: 'https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.css',
+        integrity: 'sha256-uik/hNqHWZldXh/0K35nqOSCff9F61/ZOFReqNOBgB0=',
+        crossOrigin: 'anonymous', rel: 'stylesheet'
     }
-    scripts = {
-        katex: {
-            src: 'https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.js',
-            integrity: 'sha256-6xggdIcWFnTnFwh8MX2xSsGmLa2uzMuAJJnOFzv+tzk=',
-            crossOrigin: 'anonymous'
-        },
-        katexAutoRender: {
-            src: 'https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/contrib/auto-render.min.js',
-            integrity: 'sha256-y39Mpg7V3D4lhBX4x6O0bUqTV4pSrfgwEfGKfxkOdgI=',
-            crossOrigin: 'anonymous'
-        },
-        markdownIt: {
-            src: 'https://cdn.jsdelivr.net/npm/markdown-it@13.0.1/dist/markdown-it.min.js',
-            integrity: 'sha256-hNyljag6giCsjv/yKmxK8/VeHzvMDvc5u8AzmRvm1BI=',
-            crossOrigin: 'anonymous'
-        },
-        markdownItAnchor: {
-            src: 'https://cdn.jsdelivr.net/npm/markdown-it-anchor@8.6.4/dist/markdownItAnchor.umd.js',
-            integrity: 'sha256-is0GToRxOcbAVq2XJvDAs5V0jnjYaKezgbMHdjB2r+E=',
-            crossOrigin: 'anonymous'
-        }
+}
+const SCRIPTS = {
+    katex: {
+        src: 'https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/katex.min.js',
+        integrity: 'sha256-6xggdIcWFnTnFwh8MX2xSsGmLa2uzMuAJJnOFzv+tzk=',
+        crossOrigin: 'anonymous'
+    },
+    katexAutoRender: {
+        src: 'https://cdn.jsdelivr.net/npm/katex@0.16.0/dist/contrib/auto-render.min.js',
+        integrity: 'sha256-y39Mpg7V3D4lhBX4x6O0bUqTV4pSrfgwEfGKfxkOdgI=',
+        crossOrigin: 'anonymous'
+    },
+    markdownIt: {
+        src: 'https://cdn.jsdelivr.net/npm/markdown-it@13.0.1/dist/markdown-it.min.js',
+        integrity: 'sha256-hNyljag6giCsjv/yKmxK8/VeHzvMDvc5u8AzmRvm1BI=',
+        crossOrigin: 'anonymous'
+    },
+    markdownItAnchor: {
+        src: 'https://cdn.jsdelivr.net/npm/markdown-it-anchor@8.6.4/dist/markdownItAnchor.umd.js',
+        integrity: 'sha256-is0GToRxOcbAVq2XJvDAs5V0jnjYaKezgbMHdjB2r+E=',
+        crossOrigin: 'anonymous'
     }
 }
 function Spinner(
-    state: { show: boolean, grow?: boolean }
+    props: { show: boolean, grow?: boolean }
 ) {
     const classes = [
         'spinner spinner-border spinner-border-sm', 'float-start',
-        !state.show ? 'd-none' : '', state.grow ? 'spinner-grow' : ''
+        !props.show ? 'd-none' : '', props.grow ? 'spinner-grow' : ''
     ];
     return <span
         className={classes.join(' ')}
