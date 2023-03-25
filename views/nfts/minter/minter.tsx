5217169f4926d04d8bfb42a1c9347d61381a331a
Kârūn The Rich <development@xpowermine.com>
2023-03-25 22:34:50 +0300

    XPower UI: NFTs spinner fix

--- views/nfts/minter/minter.tsx
+++ views/nfts/minter/minter.tsx
@@ -134,7 +134,7 @@ function Spinner(
     ];
     return <span
         className={classes.join(' ')} role='status'
-        style={{ visibility: show ? 'visible' : 'hidden' }}
+        style={{ display: show ? 'inline-block' : 'none' }}
     />;
 }
 function approved(
