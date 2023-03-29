fd56f4c1be87a44536507a8bcebd6aa5279e6a2c
Kârūn The Rich <development@xpowermine.com>
2023-03-30 00:14:27 +0300

    XPower UI: tests w/env-vars-fuji

--- test/env-vars.ts
+++ test/env-vars.ts
@@ -1,21 +1,4 @@
-process.env.NFT_URI = JSON.stringify({
-    'thor/333300.json': '/ipfs/QmTvy3Vfnj4UNL5dpxDcA5fLLQpwzA2LMScsEpgsbYikLg',
-    'thor': '/ipfs/QmcMVdnJV6VaicjukafsTK1pX4Zthx6JbRvC9igLScrAjD/nfts',
-    'loki/333303.json': '/ipfs/QmTvy3Vfnj4UNL5dpxDcA5fLLQpwzA2LMScsEpgsbYikLg',
-    'loki': '/ipfs/QmcMVdnJV6VaicjukafsTK1pX4Zthx6JbRvC9igLScrAjD/nfts',
-    'odin/333306.json': '/ipfs/QmTvy3Vfnj4UNL5dpxDcA5fLLQpwzA2LMScsEpgsbYikLg',
-    'odin': '/ipfs/QmcMVdnJV6VaicjukafsTK1pX4Zthx6JbRvC9igLScrAjD/nfts',
-    'hela/333309.json': '/ipfs/QmTvy3Vfnj4UNL5dpxDcA5fLLQpwzA2LMScsEpgsbYikLg',
-    'hela': '/ipfs/QmcMVdnJV6VaicjukafsTK1pX4Zthx6JbRvC9igLScrAjD/nfts',
-});
-process.env.PPT_URI = JSON.stringify({
-    'thor/333300.json': '/ipfs/QmTvy3Vfnj4UNL5dpxDcA5fLLQpwzA2LMScsEpgsbYikLg',
-    'thor': '/ipfs/QmcMVdnJV6VaicjukafsTK1pX4Zthx6JbRvC9igLScrAjD/ppts',
-    'loki/333303.json': '/ipfs/QmTvy3Vfnj4UNL5dpxDcA5fLLQpwzA2LMScsEpgsbYikLg',
-    'loki': '/ipfs/QmcMVdnJV6VaicjukafsTK1pX4Zthx6JbRvC9igLScrAjD/ppts',
-    'odin/333306.json': '/ipfs/QmTvy3Vfnj4UNL5dpxDcA5fLLQpwzA2LMScsEpgsbYikLg',
-    'odin': '/ipfs/QmcMVdnJV6VaicjukafsTK1pX4Zthx6JbRvC9igLScrAjD/ppts',
-    'hela/333309.json': '/ipfs/QmTvy3Vfnj4UNL5dpxDcA5fLLQpwzA2LMScsEpgsbYikLg',
-    'hela': '/ipfs/QmcMVdnJV6VaicjukafsTK1pX4Zthx6JbRvC9igLScrAjD/ppts',
-});
-process.env.IPFS_GATEWAY = 'https://dweb.link';
+import { join } from 'path';
+// ensure environment vars
+import dotenv from 'dotenv';
+dotenv.config({ path: join(__dirname, 'env-vars-fuji') });
