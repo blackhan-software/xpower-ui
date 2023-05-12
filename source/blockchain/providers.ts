a9727e55c14c60b06b319bd8850ad371e00a23d2
Kârūn The Rich <development@xpowermine.com>
2023-05-12 23:15:48 +0300

    XPower UI: WSProvider reconnectable

--- source/blockchain/providers.ts
+++ source/blockchain/providers.ts
@@ -1,11 +1,12 @@
 import { BrowserProvider, JsonRpcApiProvider, JsonRpcProvider, WebSocketProvider } from 'ethers';
-import { Blockchain } from './blockchain';
+import { Blockchain } from '.';
 
 import { Global } from '../types';
+import { ROParams } from '../params';
 declare const global: Global;
 
 export async function MMProvider(
-    _url?: string, ms = Number(localStorage.getItem('mmw-polling') ?? 3000)
+    polling_ms = 2000
 ): Promise<
     BrowserProvider | undefined
 > {
@@ -13,34 +14,47 @@ export async function MMProvider(
         const provider = await Blockchain.provider;
         if (provider) {
             const mm_provider = new BrowserProvider(provider);
+            mm_provider.pollingInterval = polling_ms;
             global.MM_PROVIDER = mm_provider;
-            mm_provider.pollingInterval = ms;
             return mm_provider;
         }
     }
     return global.MM_PROVIDER;
 }
-export async function MYProvider(): Promise<
+export async function MYProvider(
+    polling_ms = 2000
+): Promise<
     JsonRpcApiProvider | undefined
 > {
     if (global.MY_PROVIDER === undefined) {
         const url = MYProviderUrl();
-        if (url.startsWith('https://api.avax.network/ext/bc/C/rpc')) {
-            return global.MY_PROVIDER = await MMProvider();
-        }
-        if (url.startsWith('ws')) {
-            const wss_provider = await WSSProvider(url);
-            if (wss_provider !== undefined) {
-                return global.MY_PROVIDER = wss_provider;
+        if (url && url.startsWith('http')) {
+            const rpc_provider = await RPCProvider(url, polling_ms);
+            if (rpc_provider !== undefined) {
+                return global.MY_PROVIDER = rpc_provider;
             }
         }
-        const rpc_provider = await RPCProvider(url);
-        return global.MY_PROVIDER = rpc_provider;
+        if (url && url.startsWith('ws')) {
+            const ws_provider = await WSProvider(url, polling_ms);
+            if (ws_provider !== undefined) {
+                return global.MY_PROVIDER = ws_provider;
+            }
+        }
+        return global.MY_PROVIDER = await MMProvider();
     }
     return global.MY_PROVIDER;
 }
-async function WSSProvider(
-    url: string, ms = Number(localStorage.getItem('wss-keep-alive') ?? 3000)
+async function RPCProvider(
+    url: string, polling_ms: number
+): Promise<
+    JsonRpcProvider | undefined
+> {
+    const provider = new JsonRpcProvider(url);
+    provider.pollingInterval = polling_ms;
+    return provider;
+}
+async function WSProvider(
+    url: string, polling_ms: number
 ): Promise<
     WebSocketProvider | undefined
 > {
@@ -49,42 +63,56 @@ async function WSSProvider(
         const provider = new WebSocketProvider(
             new WebSocket(url), Number(chain_id)
         );
-        provider.once('block', () => {
-            setInterval(async function keepAlive() {
-                const tid = setTimeout(() => location.reload(), 2000);
-                await provider.getBlockNumber();
-                clearTimeout(tid);
-            }, ms);
+        provider.once('block', (number) => {
+            if (ROParams.debug > 2) {
+                console.debug('[block]', number, '!');
+            }
+            keepAlive(provider);
         });
         return provider;
     }
-}
-async function RPCProvider(
-    url: string, ms = Number(localStorage.getItem('rpc-polling') ?? 3000)
-): Promise<
-    JsonRpcProvider | undefined
-> {
-    const provider = new JsonRpcProvider(url);
-    provider.pollingInterval = ms;
-    return provider;
-}
-function MYProviderUrl(
-    url = localStorage.getItem('my-provider-url')
-) {
-    if (!url) {
-        if (typeof document === 'undefined') {
-            return 'http://127.0.0.1:9650/ext/bc/C/rpc'; // test-env
+    function keepAlive(
+        provider: WebSocketProvider
+    ) {
+        if (global.WS_PROVIDER_IID) {
+            clearInterval(global.WS_PROVIDER_IID);
+            delete global.WS_PROVIDER_IID;
         }
-        const $url = document.querySelector<HTMLElement>(
-            '#g-urls-provider-my'
-        );
-        if (!$url) {
-            throw new Error('#g-urls-provider-my missing');
-        }
-        url = $url.dataset.value ?? null;
-        if (!url) {
-            throw new Error('#g-urls-provider-my[data-value] missing');
-        }
+        global.WS_PROVIDER_IID = setInterval(async () => {
+            const tid = setTimeout(() => {
+                clearInterval(global.WS_PROVIDER_IID);
+                provider.removeAllListeners();
+                reconnect();
+            }, polling_ms);
+            const number = await provider.getBlockNumber();
+            if (ROParams.debug > 2) {
+                console.debug('[block]', number);
+            }
+            clearTimeout(tid);
+        }, polling_ms);
     }
-    return `${new URL(url)}?code=${String.random(4)}`;
+    function reconnect() {
+        global.MY_PROVIDER_OTF = undefined;
+        global.MY_PROVIDER = undefined;
+        Blockchain.reconnect();
+    }
+}
+function MYProviderUrl(): string | null {
+    if (typeof document === 'undefined') {
+        return 'http://127.0.0.1:9650/ext/bc/C/rpc'; // test-env
+    }
+    const $url = document.querySelector<HTMLElement>(
+        '#g-urls-provider-my'
+    );
+    if (!$url) {
+        throw new Error('#g-urls-provider-my missing');
+    }
+    const url = $url.dataset.value ?? null;
+    if (typeof url !== 'string') {
+        throw new Error('#g-urls-provider-my[data-value] missing');
+    }
+    if (global.MY_PROVIDER_URL === undefined && url) {
+        global.MY_PROVIDER_URL = `${new URL(url)}?code=${String.random(4)}`;
+    }
+    return global.MY_PROVIDER_URL ?? null;
 }
