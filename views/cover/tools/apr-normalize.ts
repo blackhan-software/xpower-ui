25f958915671f4e25f749acf050e43a8270a85f9
Kârūn The Rich <development@xpowermine.com>
2023-05-18 19:36:07 +0300

    XPower UI: apr-normalize fix

--- views/cover/tools/apr-normalize.ts
+++ views/cover/tools/apr-normalize.ts
@@ -1,9 +1,13 @@
+import { ROParams } from "../../../source/params";
+import { Version } from "../../../source/types";
+
 export function normalize(
     value: bigint | number
 ) {
+    const denominator = ROParams.version < Version.v7b ? 1e3 : 1e6;
     if (typeof value === 'bigint') {
-        return Number(value) / 1000;
+        return Number(value) / denominator;
     }
-    return value / 1000;
+    return value / denominator;
 }
 export default normalize;
