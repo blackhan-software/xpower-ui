242c6edbbfa638c73a4d036f911f13b73911a301
Kârūn The Rich <development@xpowermine.com>
2023-06-04 22:14:05 +0300

    XPower UI: fixed x{32,40,64}

--- source/functions/big-int/index.ts
+++ source/functions/big-int/index.ts
@@ -23,48 +23,48 @@ export function hex_plain(
     value: bigint | number, min_length: number
 ) {
     const string = value.toString(16);
-    const length = min_length - string.length;
+    const length = Math.max(min_length - string.length, 0);
     return `${'0'.repeat(length)}` + string;
 }
 export function x40(
     value: bigint | number, min_length = 40
 ) {
     const string = value.toString(16);
-    const length = min_length - string.length;
+    const length = Math.max(min_length - string.length, 0);
     return `0x${'0'.repeat(length)}` + string as Address;
 }
 export function x40_plain(
     value: bigint | number, min_length = 40
 ) {
     const string = value.toString(16);
-    const length = min_length - string.length;
+    const length = Math.max(min_length - string.length, 0);
     return `${'0'.repeat(length)}` + string;
 }
 export function x32(
     value: bigint | number, min_length = 32
 ) {
     const string = value.toString(16);
-    const length = min_length - string.length;
+    const length = Math.max(min_length - string.length, 0);
     return `0x${'0'.repeat(length)}` + string;
 }
 export function x32_plain(
     value: bigint | number, min_length = 32
 ) {
     const string = value.toString(16);
-    const length = min_length - string.length;
+    const length = Math.max(min_length - string.length, 0);
     return `${'0'.repeat(length)}` + string
 }
 export function x64(
     value: bigint | number, min_length = 64
 ) {
     const string = value.toString(16);
-    const length = min_length - string.length;
+    const length = Math.max(min_length - string.length, 0);
     return `0x${'0'.repeat(length)}` + string;
 }
 export function x64_plain(
     value: bigint | number, min_length = 64
 ) {
     const string = value.toString(16);
-    const length = min_length - string.length;
+    const length = Math.max(min_length - string.length, 0);
     return `${'0'.repeat(length)}` + string
 }
