9c3f1e658e22b71898b2ca2c33453caa6ac785dd
Kârūn The Rich <development@xpowermine.com>
2023-06-17 17:29:24 +0300

    XPower UI: fixed minting

--- source/contract/xpower-moe/xpower-moe.ts
+++ source/contract/xpower-moe/xpower-moe.ts
@@ -1,5 +1,5 @@
 import { InterfaceAbi, Transaction } from 'ethers';
-import { hex, x32, x40, x64 } from '../../functions';
+import { hex_even, x32, x40, x64 } from '../../functions';
 import { ROParams } from '../../params';
 import { Account, Address, BlockHash, Nonce } from '../../redux/types';
 import { Version } from '../../types';
@@ -27,26 +27,23 @@ export class XPowerMoe extends Base {
         const contract = await this.otf;
         if (ROParams.version < Version.v3a && !ROParams.versionFaked) {
             return contract['mint(uint256,bytes32)'](
-                x64(nonce), x64(block_hash), {
-                    gasLimit: 250_000
-                }
+                x64(nonce), x64(block_hash), this.options
             );
         }
         if (ROParams.version < Version.v7c && !ROParams.versionFaked) {
             return contract['mint(address,bytes32,uint256)'](
-                x40(to), x64(block_hash), x64(nonce), {
-                    gasLimit: 250_000
-                }
+                x40(to), x64(block_hash), x64(nonce), this.options
             );
         }
         return contract['mint(address,bytes16,bytes)'](
-            x40(to), x32(block_hash), hex(nonce), {
-                gasLimit: 250_000
-            }
+            x40(to), x32(block_hash), hex_even(nonce), this.options
         );
     }
     private get otf() {
         return OtfManager.connect(this.connect());
+    }
+    private get options() {
+        return { gasLimit: 250_000 };
     }
 }
 export default XPowerMoe;
