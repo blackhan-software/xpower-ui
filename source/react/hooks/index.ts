7481ee264bd98ddf9b7646a7181519910ca16b0e
Kârūn The Rich <development@xpowermine.com>
2023-06-29 22:39:23 +0300

    XPower UI: useDoubleTap hook export

--- source/react/hooks/index.ts
+++ source/react/hooks/index.ts
@@ -2,6 +2,7 @@ export * from './use-buffered-effect';
 export * from './use-buffered-if';
 export * from './use-delayed-effect';
 export * from './use-delayed-if';
+export * from './use-double-tap';
 export * from './use-mouse-drag';
 export * from './use-mouse-drag-x';
 export * from './use-mouse-drag-y';
