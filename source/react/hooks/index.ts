a775ed754ff29c0dc7eb280b051b7c4378a8756d
Kârūn The Rich <development@xpowermine.com>
2023-06-30 19:27:29 +0300

    XPower UI: use-long-tap react-hook

--- source/react/hooks/index.ts
+++ source/react/hooks/index.ts
@@ -3,6 +3,7 @@ export * from './use-buffered-if';
 export * from './use-delayed-effect';
 export * from './use-delayed-if';
 export * from './use-double-tap';
+export * from './use-long-tap';
 export * from './use-mouse-drag';
 export * from './use-mouse-drag-x';
 export * from './use-mouse-drag-y';
---
+++ source/react/hooks/use-long-tap.ts
@@ -0,0 +1,56 @@
+import { useState, useEffect, useCallback, useRef } from 'react';
+
+export interface OnLongTap {
+    (e?: MouseEvent | TouchEvent): void
+}
+export function useLongTap<T extends HTMLElement>(
+    $ref: React.RefObject<T>, handler: OnLongTap, ms = 900
+): [
+    boolean, (flag: boolean) => void
+] {
+    const timer = useRef<NodeJS.Timeout | undefined>();
+    const [stamp, set_stamp] = useState(0);
+    const tapped = useRef<boolean>(false);
+    const start = useCallback(() => {
+        timer.current = setTimeout(() => {
+            if (performance.now() - stamp >= ms) {
+                tapped.current = true;
+                handler(undefined);
+            }
+        }, ms);
+        tapped.current = false;
+        set_stamp(performance.now());
+    }, [
+        handler, stamp, ms
+    ]);
+    const end = useCallback(() => {
+        if (timer.current) {
+            clearTimeout(timer.current);
+            timer.current = undefined;
+        }
+        setTimeout(() => {
+            tapped.current = false;
+        });
+        set_stamp(0);
+    }, []);
+    useEffect(() => {
+        const el = $ref.current;
+        el?.addEventListener('mousedown', start);
+        el?.addEventListener('mouseup', end);
+        el?.addEventListener('touchstart', start);
+        el?.addEventListener('touchend', end);
+        return () => {
+            el?.removeEventListener('mousedown', start);
+            el?.removeEventListener('mouseup', end);
+            el?.removeEventListener('touchstart', start);
+            el?.removeEventListener('touchend', end);
+        };
+    }, [
+        $ref, start, end
+    ]);
+    return [
+        tapped.current, (flag) => tapped.current = flag
+    ];
+}
+
+export default useLongTap;
