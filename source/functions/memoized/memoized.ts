ff243f9b508d6bb59f7ce2e3f0fb42883e021607
Kârūn The Rich <development@xpowermine.com>
2024-02-02 19:09:26 +0300

    XPower UI: LocalStorage extends Map

--- source/functions/memoized/memoized.ts
+++ source/functions/memoized/memoized.ts
@@ -4,22 +4,49 @@ export type Memoized<F extends (...args: any[]) => any> = {
     (...args: Parameters<F>): ReturnType<F>;
 };
 export function memoized<F extends (...args: any[]) => any>(
-    fn: F, keyOf?: (...args: Parameters<F>) => MemoizedKey
+    fn: F, keyOf?: (...args: Parameters<F>) => MemoizedKey,
+    getCache?: () => Map<MemoizedKey, ReturnType<F>>
 ): Memoized<F> {
-    const cache = new Map<MemoizedKey, ReturnType<F>>();
-    const memoized = function (
+    if (getCache === undefined) {
+        getCache = () => new Map<MemoizedKey, ReturnType<F>>();
+    }
+    const cache = getCache();
+    const memoized = async function (
         this: any, ...args: Parameters<F>
-    ): ReturnType<F> {
+    ): Promise<ReturnType<F>> {
         const key = keyOf
             ? keyOf.apply(this, args) : JSON.stringify(args);
         const cached = cache.get(key);
         if (cached !== undefined) {
             return cached;
         }
-        const result = fn.apply(this, args);
+        const result = await fn.apply(this, args);
         cache.set(key, result);
         return result;
     };
     return memoized as Memoized<F>;
+}
+export class LocalStorage<K extends string, V> extends Map<K, V> {
+    get(key: K): V | undefined {
+        const item = localStorage.getItem(key);
+        if (item !== null) return JSON.parse(item);
+    }
+    set(key: K, value: V) {
+        const json = JSON.stringify(value);
+        localStorage.setItem(key, json);
+        return this;
+    }
+    delete(key: K): boolean {
+        const item = localStorage.getItem(key);
+        localStorage.removeItem(key);
+        return item !== null;
+    }
+    has(key: K): boolean {
+        const item = localStorage.getItem(key);
+        return item !== null;
+    }
+    clear() {
+        localStorage.clear();
+    }
 }
 export default memoized;
