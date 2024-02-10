7daa4710842788975e983985145b939acf3a2bbd
Kârūn The Rich <development@xpowermine.com>
2024-02-10 21:10:43 +0300

    XPower UI: GitHub actions fix

--- test/env-vars.ts
+++ test/env-vars.ts
@@ -2,3 +2,9 @@ import { join } from 'path';
 // ensure environment vars
 import dotenv from 'dotenv';
 dotenv.config({ path: join(__dirname, 'env-vars-fuji') });
+// try importing '.env' config
+try {
+    dotenv.config({ path: join(__dirname, '..', '.env') });
+} catch (ex) {
+    console.error(ex);
+}
